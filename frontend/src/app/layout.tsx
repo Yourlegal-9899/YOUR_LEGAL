585de2935e038709009a73c92f940fd0044fc097
HoneyBharti <honeybharti920@gmail.com>
2026-04-24 16:48:36 +0530

    pixelcode

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -63,9 +63,32 @@ export default function RootLayout({
             gtag('config', 'G-H58ZS2N8JB');
           `}
         </script>
+        <Script id="meta-pixel" strategy="beforeInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1436250777767419');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         <OrganizationSchema />
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1436250777767419&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <FirebaseClientProvider>
           <AuthProvider>
             {children}
